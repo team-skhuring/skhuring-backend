92669be30956331a449265ef5f905214994a361d
dldlscjf123 <dldlscjf1231@naver.com>
2025-03-31 19:24:37 +0900

    feat : Memo 엔티티 작성

--- src/main/java/com/skhuring/mentoring/domain/Memo.java
+++ src/main/java/com/skhuring/mentoring/domain/Memo.java
@@ -1,6 +1,27 @@
 package com.skhuring.mentoring.domain;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long chat_room_id;
+
+    private String content;
+
 }
