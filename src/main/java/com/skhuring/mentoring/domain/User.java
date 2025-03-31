f68f106602b23c61a3eb2cad36898d4a94aa9f7c
dldlscjf123 <dldlscjf1231@naver.com>
2025-03-31 13:09:20 +0900

    feat : User 엔티티 작성

---
+++ src/main/java/com/skhuring/mentoring/domain/User.java
@@ -0,0 +1,31 @@
+package com.skhuring.mentoring.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email; //카카오는 이메일이 안된다고 했음 일단 넣어둠
+
+    @Column(nullable = false)
+    private String socialType;
+
+    @Column(nullable = false, unique = true)
+    private String socialId;
+
+}
