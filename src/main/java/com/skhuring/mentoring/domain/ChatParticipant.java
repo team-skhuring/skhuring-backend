9bd9444964e2103325c889b67716411de2d44ec6
dldlscjf123 <dldlscjf1231@naver.com>
2025-03-31 19:23:24 +0900

    feat : ChatParticipant 엔티티 작성

--- src/main/java/com/skhuring/mentoring/domain/ChatParticipant.java
+++ src/main/java/com/skhuring/mentoring/domain/ChatParticipant.java
@@ -1,4 +1,30 @@
 package com.skhuring.mentoring.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class ChatParticipant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    private String content;
+
+
 }
