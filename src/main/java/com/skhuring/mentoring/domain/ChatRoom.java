72ed0db7b483baef5c88dbfe54b7e253ff0f28a2
dldlscjf123 <dldlscjf1231@naver.com>
2025-03-31 19:24:04 +0900

    feat : ChatRoom 엔티티 작성

--- src/main/java/com/skhuring/mentoring/domain/ChatRoom.java
+++ src/main/java/com/skhuring/mentoring/domain/ChatRoom.java
@@ -1,4 +1,41 @@
 package com.skhuring.mentoring.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Column(nullable = false)
+    private boolean anonymous;
+
+    private static final int MAX_PARTICIPANTS = 4;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime created_at;
+
+    private boolean mentor_status;
+
+
+
 }
