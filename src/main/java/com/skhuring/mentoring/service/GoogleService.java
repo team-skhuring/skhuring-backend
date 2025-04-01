913647f490c9bce6c27e43b4c25ac3801ac45c08
dldlscjf123 <dldlscjf1231@naver.com>
2025-04-02 00:25:56 +0900

    feat : GoogleService 생성 구글 로그인 관련 서비스 클래스

---
+++ src/main/java/com/skhuring/mentoring/service/GoogleService.java
@@ -0,0 +1,43 @@
+package com.skhuring.mentoring.service;
+
+import com.skhuring.mentoring.dto.AccessTokenDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
+@Slf4j
+@Service
+public class GoogleService {
+    @Value("${oauth.google.client-id}")
+    private String googleClientId;
+
+    @Value("${oauth.google.client-secret}")
+    private String googleClientSecret;
+
+    @Value("${oauth.google.redirect-uri}")
+    private String googleRedirectUri;
+
+    public AccessTokenDto getAccessToken(String code) {
+        RestClient restClient = RestClient.create();
+
+//        MultiValueMap을 통해 자동으로 form-data형식으로 body 조립 가능
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("code", code);
+        params.add("client_id", googleClientId);
+        params.add("client_secret", googleClientSecret);
+        params.add("redirect_uri", googleRedirectUri);
+        params.add("grant_type", "authorization_code");
+
+        ResponseEntity<AccessTokenDto> response =  restClient.post()
+                .uri("https://oauth2.googleapis.com/token")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .body(params)
+                .retrieve()
+                .toEntity(AccessTokenDto.class);
+        log.info("Access Token: {}", response.getBody());
+        return response.getBody();
+    }
+}
