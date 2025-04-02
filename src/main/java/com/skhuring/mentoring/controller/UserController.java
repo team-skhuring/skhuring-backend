4587089e826be3bd46ee27bd70c48dabd9dc3b1e
dldlscjf123 <dldlscjf1231@naver.com>
2025-04-02 10:51:28 +0900

    feat : UserController googleLogin메서드 구현 엑세스 토큰 발급, 사용자 정보 얻기, 회원등록 id, token반환

--- src/main/java/com/skhuring/mentoring/controller/UserController.java
+++ src/main/java/com/skhuring/mentoring/controller/UserController.java
@@ -1,27 +1,55 @@
 package com.skhuring.mentoring.controller;
 
+import com.skhuring.mentoring.common.auth.JwtTokenProvider;
+import com.skhuring.mentoring.domain.SocialType;
+import com.skhuring.mentoring.domain.User;
 import com.skhuring.mentoring.dto.AccessTokenDto;
+import com.skhuring.mentoring.dto.GoogleProfileDto;
 import com.skhuring.mentoring.dto.RedirectDto;
 import com.skhuring.mentoring.service.GoogleService;
+import com.skhuring.mentoring.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/member")
 
 public class UserController {
     private final GoogleService googleService;
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public UserController(GoogleService googleService) {
+    public UserController(GoogleService googleService, UserService userService, JwtTokenProvider jwtTokenProvider) {
         this.googleService = googleService;
+        this.userService = userService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/google/doLogin")
-    public void googleLogin(@RequestBody RedirectDto redirectDto) {
+    public ResponseEntity<?> googleLogin(@RequestBody RedirectDto redirectDto) {
+//        액세스 토큰 발급
        AccessTokenDto accessTokenDto =  googleService.getAccessToken(redirectDto.getCode());
+//       사용자 정보 얻기
+       GoogleProfileDto googleProfileDto =  googleService.getGoogleProfile(accessTokenDto.getAccess_token());
+//       회원가입이 되어있지 않다면 회원가입
+        User originalMember = userService.getMemberBySocialId(googleProfileDto.getSub());
+        if(originalMember == null){
+            originalMember = userService.createOauth(googleProfileDto.getSub(), googleProfileDto.getEmail(), SocialType.GOOGLE);
+        }
+//        회원등록되어있다면 jwttoken발급
+        String jwtToken = jwtTokenProvider.createToken(originalMember.getEmail());
+
+        Map<String, Object> loginInfo = new HashMap<>();
+        loginInfo.put("id", originalMember.getId());
+        loginInfo.put("token", jwtToken);
+        return new ResponseEntity<>(loginInfo, HttpStatus.OK);
     }
 }
