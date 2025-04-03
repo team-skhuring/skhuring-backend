ed3400da5cb7a13444a2561e00f38b9bd30ad10f
dldlscjf123 <dldlscjf1231@naver.com>
2025-04-03 14:13:40 +0900

    feat : GoogleProfileDto 생성 구글 사용자정보 얻는 Dto클래스 , sub, email, picture

---
+++ src/main/java/com/skhuring/mentoring/dto/GoogleProfileDto.java
@@ -0,0 +1,16 @@
+package com.skhuring.mentoring.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GoogleProfileDto {
+    private String sub; //사용자 고유 아이디 subjectId
+    private String email;
+    private String picture;
+}
