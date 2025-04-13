cebcc1cf57bae5d12a8329f7ad90c1940bdc3cce
dldlscjf123 <dldlscjf1231@naver.com>
2025-04-13 12:06:17 +0900

    feat : SecurityFulterChain 인증필터 추가 jwt 인증 필터 추가

--- src/main/java/com/skhuring/mentoring/common/config/SecurityConfig.java
+++ src/main/java/com/skhuring/mentoring/common/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.skhuring.mentoring.common.config;
 
+import com.skhuring.mentoring.common.auth.JwtAuthFilter;
+import com.skhuring.mentoring.common.auth.JwtTokenProvider;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,16 +17,20 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 
 @Configuration
+@AllArgsConstructor
 public class SecurityConfig {
 
+    private JwtAuthFilter jwtAuthFilter;
+
     @Bean
-    public SecurityFilterChain myfilter(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain myfilter(HttpSecurity httpSecurity, JwtTokenProvider jwtTokenProvider) throws Exception {
         return httpSecurity
                 .cors(cors -> cors.configurationSource(configurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(a->a.requestMatchers("/member/google/doLogin" , "/oauth2/**").permitAll().anyRequest().authenticated())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
