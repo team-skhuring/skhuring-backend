66089891853f948b09e9456e91343a4b46c55de8
dldlscjf123 <dldlscjf1231@naver.com>
2025-04-14 22:34:56 +0900

    feat : StompWebSocketConfig 생성

--- src/main/java/com/skhuring/mentoring/common/config/StompWebSocketConfig.java
+++ src/main/java/com/skhuring/mentoring/common/config/StompWebSocketConfig.java
@@ -1,4 +1,27 @@
 package com.skhuring.mentoring.common.config;
 
-public class StompWebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/connect")
+                .setAllowedOrigins("http://localhost:5173")
+                .withSockJS();
+
+    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        /publish/1 형태로 메시지 발행
+//        /publish행태로 시작하는 url패턴으로 메시지가 발행되면 @Controller 객체의 @MessageMapping메서드로 라우팅
+        registry.setApplicationDestinationPrefixes("/publish");
+//        /topic/1 형태로 메시지를 수신해야함을 설정
+        registry.enableSimpleBroker("/topic");
+    }
 }
